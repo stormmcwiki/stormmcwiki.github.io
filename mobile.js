b69c7b4faf9a9cba52dfa1e5c9ee5cb9647a7041
StormMC <86464607+stormmcwiki@users.noreply.github.com>
2024-02-25 14:48:05 +0800

    Create mobile.js

---
+++ mobile.js
@@ -0,0 +1,24 @@
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+function scrollToElement(elementSelector, instance = 0) {
+    const elements = document.querySelectorAll(elementSelector);
+    if (elements.length > instance) {
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+function scrollToBottom(){
+  window.scrollTo(0,10000);
+  overlay.classList.remove("overlay--active") 
+}
